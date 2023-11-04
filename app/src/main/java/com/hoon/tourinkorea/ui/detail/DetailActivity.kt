e4b8fce217ab58ee2d7dc87cf46776f6daf76823
gnsdl5253 <gnsdl5253@naver.com>
2023-11-04 16:45:26 +0900

    feat: Detail추가

---
+++ app/src/main/java/com/hoon/tourinkorea/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.hoon.tourinkorea.ui.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hoon.tourinkorea.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
