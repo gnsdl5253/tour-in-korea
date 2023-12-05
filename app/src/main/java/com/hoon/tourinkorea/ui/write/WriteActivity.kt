8118e2e3ffce45c0635013699f20472bebc1f858
gnsdl5253 <gnsdl5253@naver.com>
2023-12-06 00:32:02 +0900

    feat: write에서 작성버튼 클릭시 DB업로드전에 홈화면으로 이동해 업데이트 되지않는 문제 해결

--- app/src/main/java/com/hoon/tourinkorea/ui/write/WriteActivity.kt
+++ app/src/main/java/com/hoon/tourinkorea/ui/write/WriteActivity.kt
@@ -2,6 +2,9 @@ package com.hoon.tourinkorea.ui.write
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
@@ -12,7 +15,10 @@ import androidx.recyclerview.widget.ConcatAdapter
 import com.hoon.tourinkorea.MainActivity
 import com.hoon.tourinkorea.databinding.ActivityWriteBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class WriteActivity : AppCompatActivity() {
@@ -21,16 +27,27 @@ class WriteActivity : AppCompatActivity() {
     private lateinit var imageCountAdapter: ImageCountAdapter
     private lateinit var postSelectAdapter: PostSelectAdapter
     private val viewModel: WriteViewModel by viewModels()
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        progressBar = binding.progressBar
+
         setupAdapters()
         setupRecyclerView()
         setupUI()
         observeViewModel()
+
+        val roadAddressName = intent.getStringExtra("roadAddressName")
+        Log.d("WriteActivity", "Received placeName: $roadAddressName")
+        if (!roadAddressName.isNullOrEmpty()) {
+            binding.etWriteLocation.post {
+                binding.etWriteLocation.setText(roadAddressName)
+            }
+        }
     }
 
     private fun setupAdapters() {
@@ -61,21 +78,35 @@ class WriteActivity : AppCompatActivity() {
         binding.toolBarWrite.setNavigationOnClickListener {
             finish()
         }
+        binding.etWriteLocation.setOnClickListener {
+
+        }
 
         binding.btnWrite.setOnClickListener {
             val title = binding.etWriteTitle.text.toString()
             val location = binding.etWriteLocation.text.toString()
             val description = binding.etWriteDescription.text.toString()
 
+
             if (title.isEmpty() || location.isEmpty() || description.isEmpty() || viewModel.selectedImageUris.value.isEmpty()) {
                 Toast.makeText(this, "이미지와 내용을 모두 입력해주세요.", Toast.LENGTH_SHORT).show()
             } else {
-                viewModel.updateTitle(title)
-                viewModel.updateLocation(location)
-                viewModel.updateDescription(description)
-                viewModel.createPost()
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+                showProgressBar()
+
+                lifecycleScope.launch {
+                    viewModel.updateTitle(title)
+                    viewModel.updateLocation(location)
+                    viewModel.updateDescription(description)
+                    viewModel.createPost()
+
+                    delay(2000)
+
+                    withContext(Dispatchers.Main) {
+                        hideProgressBar()
+                        val intent = Intent(this@WriteActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
             }
         }
     }
@@ -83,10 +114,19 @@ class WriteActivity : AppCompatActivity() {
     private fun observeViewModel() {
         lifecycleScope.launch {
             viewModel.selectedImageUris.collect {
+                Log.d("WriteActivity", "Selected image URIs: $it")
                 imageCountAdapter.updateData(it.size)
                 postSelectAdapter.updateData(it)
                 binding.rvWritePhoto.adapter?.notifyDataSetChanged()
             }
         }
+    }
+
+    private fun showProgressBar() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        progressBar.visibility = View.GONE
     }
 }
